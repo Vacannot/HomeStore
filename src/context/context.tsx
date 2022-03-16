dc4df60126d23333d534580b7472196b5f30d875
linnea <linnea.albertsson@gmail.com>
2022-03-16 10:31:17 +0100

    context TEST

---
+++ src/context/context.tsx
@@ -0,0 +1,27 @@
+import React, { createContext } from "react";
+
+const Cart = createContext(undefined);
+
+async function fetchMockData() {
+    const response = await fetch('https://fakestoreapi.com/products/category/jewelery')
+        .then(res=>res.json())
+        .then(json=>console.log(json))
+        return;
+}
+
+const Context = ({ children }) => {
+    
+    
+    //  products = [...Array(20)].map(() => ({
+    //     id: '',
+    //     name: '',
+    //     price: '',
+    //     image: '',
+
+    // }))
+    return(
+        // <Cart.Provider>{children}</Cart.Provider>
+    )
+}
+
+export default Context;
