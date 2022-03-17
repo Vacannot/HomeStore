1518e9745a11d5b14322b299b2c80ede3a86a31d
Camilla <73700387+millifrill@users.noreply.github.com>
2022-03-17 13:52:59 +0100

    Pascalcase bug

--- src/pages/cartPage.tsx
+++ src/pages/cartPage.tsx
@@ -1,36 +1,34 @@
-
-
-import { Button, Typography, useTheme, Theme } from "@mui/material";
-import React, { CSSProperties } from "react";
-import CartItemCard from "../components/cartItemCard";
+import React, { CSSProperties } from 'react';
+import { Button, Typography } from '@mui/material';
+import CartItemCard from '../components/CartItemCard';
 
 function CartPage() {
-  
-
-
-
-    return(
-        <div style={rootStyle}>
-            <Typography align="center" variant="h4" sx={{ m:1 }}>Shopping Cart</Typography>
-            <CartItemCard/>
-            <CartItemCard/>
-            <div style={paymentDivStyle}>
-                <Button sx={{backgroundColor: "green", m: 1}} variant="contained">GO TO PAYMENT</Button>
-                <Typography variant="h6">Total: Kr</Typography>
-            </div>
-        </div>
-    )
+	return (
+		<div style={rootStyle}>
+			<Typography align='center' variant='h4' sx={{ m: 1 }}>
+				Shopping Cart
+			</Typography>
+			<CartItemCard />
+			<CartItemCard />
+			<div style={paymentDivStyle}>
+				<Button sx={{ backgroundColor: 'green', m: 1 }} variant='contained'>
+					GO TO PAYMENT
+				</Button>
+				<Typography variant='h6'>Total: Kr</Typography>
+			</div>
+		</div>
+	);
 }
 
 const rootStyle: CSSProperties = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
 	marginTop: '4rem',
 };
 
 const paymentDivStyle: CSSProperties = {
-    maxWidth: '20rem', 
-}
+	maxWidth: '20rem',
+};
 
 export default CartPage;
