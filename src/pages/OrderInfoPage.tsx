a1109aadb17432dcb8c7e7d397557611b2f07866
linnea <linnea.albertsson@gmail.com>
2022-03-18 16:38:16 +0100

    Added basic styling

--- src/pages/OrderInfoPage.tsx
+++ src/pages/OrderInfoPage.tsx
@@ -1,17 +1,17 @@
-import React, { CSSProperties, CSSProperties } from 'react';
+import React, { CSSProperties } from 'react';
 import { Button, Container, Typography, } from '@mui/material';
 import Logo from '../components/Logo'
 
 const OrderInfoPage = () => {
     return(
         <Container>
-            <Typography variant='h5' sx={{m: 5}}>Tack för din beställning!</Typography>
-            <div>
+            <Typography variant='h5' sx={{mt: 10, mb: 3}}>Tack för din beställning!</Typography>
+            <Container>
                 <Typography variant='body1'>Ordernummer: 00000</Typography>
                 <Typography variant='body1'>
                     Bekräftelsemail och kvitto skickars till mail adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </Typography>
-            </div>
+            </Container>
             <Container>
                 {/* map() items in cart */}
                 <div style={productInfoDivStyle}>
@@ -21,16 +21,16 @@ const OrderInfoPage = () => {
                
             </Container>
             <Container>
-                <div style={productInfoDivStyle}>
+                <div style={priceInfoStyle}>
                     <p>Frakt:</p>
                     <p>Fraktsätt</p>
                     <p>59kr</p>
                 </div>
-                <div style={productInfoDivStyle}>
+                <div style={priceInfoStyle}>
                     <p>Totalt:</p>
                     <p>428kr</p>
                 </div>
-                <div style={productInfoDivStyle}>
+                <div style={priceInfoStyle}>
                     <p>Varav Moms:</p>
                     <p>29kr</p>
                 </div>
@@ -55,10 +55,10 @@ const OrderInfoPage = () => {
 }
 
 const ButtonStyle: CSSProperties = {
+    display: 'block',
     backgroundColor: "#BFD8D5",
-    marginTop: "2rem",
+    margin: '.5rem auto',
     padding: "0.5rem",
-    width: "8rem",
     boxShadow: "none",
 };
 
@@ -67,6 +67,12 @@ const productInfoDivStyle: CSSProperties = {
     justifyContent: 'space-between',
     border: '2px solid grey',
     background: '#f3f3f3'
+};
+
+const priceInfoStyle: CSSProperties = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    background: 'grey'
 };
 
 export default OrderInfoPage;
