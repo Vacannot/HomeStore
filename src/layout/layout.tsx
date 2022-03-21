2a66055601c01af043cfcd177a7ccd750ba61e0a
Camilla <73700387+millifrill@users.noreply.github.com>
2022-03-21 20:51:59 +0100

    files that refuse to be pascal

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,10 +1,12 @@
 import React, { CSSProperties } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import StartPage from '../pages/startPage';
+import StartPage from '../pages/StartPage';
 import Header from '../components/Header';
-import CartPage from '../pages/cartPage';
-import CheckoutPage from '../pages/checkoutPage';
-import Footer from '../components/Footer';
+import CartPage from '../pages/CartPage';
+import FormPage from '../pages/FormPage';
+import Footer from './footer';
+import ShippingPage from '../pages/ShippingPage';
+import OrderInfoPage from '../pages/OrderInfoPage';
 
 function Layout() {
 	return (
@@ -12,8 +14,10 @@ function Layout() {
 			<Header />
 			<Switch>
 				<Route path='/' exact component={StartPage} />
-				<Route path='/cart-page' component={CartPage} />
-				<Route path='/checkout-page' component={CheckoutPage} />
+				<Route path='/cartpage' component={CartPage} />
+				<Route path='/formpage' component={FormPage} />
+				<Route path='/shippingpage' component={ShippingPage} />
+				<Route path='/orderinfopage' component={OrderInfoPage} />
 			</Switch>
 			<Footer />
 		</div>
