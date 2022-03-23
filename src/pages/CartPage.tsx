b8607c6c90620ab6596bdc2de336197697353c21
Camilla <73700387+millifrill@users.noreply.github.com>
2022-03-23 09:45:08 +0100

    layout bug

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,40 +1,42 @@
-
-
-import { Button, Typography, useTheme, Theme } from "@mui/material";
-import React, { CSSProperties } from "react";
-import CartItemCard from "../components/cartItemCard";
-import { Link } from "react-router-dom";
-import { CartContext } from '../context/CartContext'
+import { Button, Typography, useTheme, Theme } from '@mui/material';
+import React, { CSSProperties } from 'react';
+import CartItemCard from '../components/CartItemCard';
+import { Link } from 'react-router-dom';
+// import { CartContext } from '../context/CartContext';
 
 function CartPage() {
-    const { cart } = CartContext();
+	// const { cart } = CartContext();
 
-    return(
-        <div style={rootStyle}>
-            <Typography align="center" variant="h4" sx={{ m:1 }}>Shopping Cart</Typography>
+	return (
+		<div style={rootStyle}>
+			<Typography align='center' variant='h4' sx={{ m: 1 }}>
+				Shopping Cart
+			</Typography>
 
-            <CartItemCard/>
-            <CartItemCard/>
-            <div style={paymentDivStyle}> 
-            <Link to="/formpage">
-            <Button sx={{backgroundColor: "green", m: 1}} variant="contained">GO TO PAYMENT</Button>
-            </Link>
+			<CartItemCard />
+			<CartItemCard />
+			<div style={paymentDivStyle}>
+				<Link to='/formpage'>
+					<Button sx={{ backgroundColor: 'green', m: 1 }} variant='contained'>
+						GO TO PAYMENT
+					</Button>
+				</Link>
 
-                <Typography variant="h6">Total: Kr</Typography>
-            </div>
-        </div>
-    )
+				<Typography variant='h6'>Total: Kr</Typography>
+			</div>
+		</div>
+	);
 }
 
 const rootStyle: CSSProperties = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
 	marginTop: '4rem',
 };
 
 const paymentDivStyle: CSSProperties = {
-    maxWidth: '20rem', 
-}
+	maxWidth: '20rem',
+};
 
 export default CartPage;
