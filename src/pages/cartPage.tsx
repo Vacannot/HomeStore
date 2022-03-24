ca4eed19d834f07263365a2d1158ca7cf16a7405
linnea <linnea.albertsson@gmail.com>
2022-03-24 14:13:33 +0100

    casing dont work

--- src/pages/cartPage.tsx
+++ src/pages/cartPage.tsx
@@ -1,21 +1,16 @@
 import { Button, Typography } from '@mui/material';
-import React, { CSSProperties, useContext } from 'react';
+import React, { CSSProperties } from 'react';
+import CartItemCard from '../components/CartItemCard';
 import { Link } from 'react-router-dom';
-import CartItemCard from '../components/cartItemCard';
-import { CartContext } from '../context/CartContext';
 
 function CartPage() {
-	let { cart } = useContext(CartContext)
 	return (
 		<div style={rootStyle}>
 			<Typography align='center' variant='h4' sx={{ m: 1 }}>
 				Shopping Cart
 			</Typography>
-			{cart.map((cartItem) => {
-				return (
-					<CartItemCard key={cartItem.product.id}/>
-				)
-			})}
+			<CartItemCard />
+			<CartItemCard />
 			<div style={paymentDivStyle}>
 				<Link to='/formpage'>
 					<Button sx={{ backgroundColor: 'green', m: 1 }} variant='contained'>
