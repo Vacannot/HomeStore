76c210960e4dba1644894f171719aa191b1387c4
linnea <linnea.albertsson@gmail.com>
2022-03-24 09:29:43 +0100

    -

--- src/components/productGrid.tsx
+++ src/components/productGrid.tsx
@@ -21,8 +21,8 @@ export default function ProductGrid() {
 			<Box sx={{ flexGrow: 1 }}>
 				<Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 1, sm: 6, md: 8, lg: 12 }}>
 					{products.map((product, id) => (
-						<Grid key={product.id} style={gridItem} item xs={12} sm={6} md={4} lg={3}>
-							<ProductCard  product={product} />
+						<Grid style={gridItem} item xs={12} sm={6} md={4} lg={3}>
+							<ProductCard key={id} product={product} />
 						</Grid>
 					))}
 				</Grid>
--- src/components/quantityCounter.tsx
+++ src/components/quantityCounter.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 import { useState, CSSProperties } from 'react';
@@ -7,32 +7,15 @@ import { useState, CSSProperties } from 'react';
 
 
 function QuantityCounter() {
-    const [quantity, setQuantity] = useState(1);
-    const [disabled, setDisabled] = useState();
-    
-
-    const handleOnReduce = () => {
-        // quantity === 1 ? setQuantity(1) : 
-        if (quantity === 1) {
-            setQuantity(1);
-            setDisabled(disabled);
-        }
-        
-        setQuantity(quantity - 1);
-    }
-
-    const handleOnAdd = () => {
-        setDisabled(undefined);
-        setQuantity(quantity + 1)
-    }
+    const [quantity, setQuantity] = useState(0);
 
     return(
         <div style={iconsDivStyle}>
-            <Button {...disabled}>
-                <RemoveCircleIcon onClick={handleOnReduce} />
-            </Button>
+            <RemoveCircleIcon onClick={() => 
+                quantity === 0 ? setQuantity(0) : setQuantity(quantity - 1)}
+            />
             <Typography>Antal {quantity}</Typography>
-            <AddCircleIcon onClick={handleOnAdd}/>
+            <AddCircleIcon onClick={() => setQuantity(quantity + 1)}/>
         </div>
         
     )
