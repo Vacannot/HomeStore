0fff4e0074b7ae10e6f3ad5ec2a47ae3434e0b5f
Camilla <73700387+millifrill@users.noreply.github.com>
2022-03-26 20:19:51 +0100

    added layout after removal bc of casing bug

---
+++ src/layout/layout.tsx
@@ -0,0 +1,29 @@
+import React, { CSSProperties } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import StartPage from '../pages/startPage';
+import Header from '../components/Header';
+import CartPage from '../pages/cartPage';
+import Footer from '../components/Footer';
+import OrderInfoPage from '../pages/orderInfoPage';
+
+function Layout() {
+	return (
+		<div style={rootStyle}>
+			<Header />
+			<Switch>
+				<Route path='/' exact component={StartPage} />
+				<Route path='/cartpage' component={CartPage} />
+				<Route path='/orderinfopage' component={OrderInfoPage} />
+			</Switch>
+			<Footer />
+		</div>
+	);
+}
+
+const rootStyle: CSSProperties = {
+	height: '100vh',
+	display: 'flex',
+	flexDirection: 'column',
+};
+
+export default Layout;
