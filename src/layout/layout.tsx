49e5537096f1e30b2412887b16ae4a4afa212b75
Emil <emil.hagelin@medieinstitutet.se>
2022-03-30 10:21:22 +0200

    camelcase problem

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,29 +1,29 @@
-import React, { CSSProperties } from "react";
-import { Switch, Route } from "react-router-dom";
-import StartPage from "../pages/StartPage";
-import Header from "../components/Header";
-import CartPage from "../pages/cartPage";
-import Footer from "../components/Footer";
-import CheckoutPage from "../pages/checkoutPage";
+import React, { CSSProperties } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import StartPage from '../pages/startPage';
+import Header from '../components/Header';
+import CartPage from '../pages/cartPage';
+import Footer from '../components/Footer';
+import CheckoutPage from '../pages/checkoutPage';
 
 function Layout() {
-  return (
-    <div style={rootStyle}>
-      <Header />
-      <Switch>
-        <Route path="/" exact component={StartPage} />
-        <Route path="/cartpage" component={CartPage} />
-        <Route path="/formpage" component={CheckoutPage} />
-      </Switch>
-      <Footer />
-    </div>
-  );
+	return (
+		<div style={rootStyle}>
+			<Header />
+			<Switch>
+				<Route path='/' exact component={StartPage} />
+				<Route path='/cartpage' component={CartPage} />
+				<Route path='/formpage' component={CheckoutPage} />
+			</Switch>
+			<Footer />
+		</div>
+	);
 }
 
 const rootStyle: CSSProperties = {
-  height: "100vh",
-  display: "flex",
-  flexDirection: "column",
+	height: '100vh',
+	display: 'flex',
+	flexDirection: 'column',
 };
 
 export default Layout;
