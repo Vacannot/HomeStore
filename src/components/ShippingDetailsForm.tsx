0c285a3f3ba4722e8ef88658eff2217f7a9b861f
Emil <emil.hagelin@medieinstitutet.se>
2022-03-30 16:05:02 +0200

    added validation to forms

--- src/components/ShippingDetailsForm.tsx
+++ src/components/ShippingDetailsForm.tsx
@@ -4,20 +4,21 @@ import { useFormik } from "formik";
 import * as yup from "yup";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 interface ShippingDetailsFormValues {
-firstName: string;
-lastName: string;
-email: string;
-number: number;
-adress: string;
-zipcode: number;
-country: string;
-city: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  number: number;
+  adress: string;
+  zipcode: number;
+  country: string;
+  city: string;
 }
 
 const validationSchema = yup.object({
-  firstnName: yup.string().required("Please enter first name").min(2),
+  firstName: yup.string().required("Please enter first name").min(2),
   lastName: yup.string().required("Please enter last name").min(2),
   email: yup
     .string()
@@ -27,7 +28,7 @@ const validationSchema = yup.object({
   address: yup.string().required("Enter your adress").min(8),
   zipcode: yup.number().required("Enter your zipcode").min(5),
   city: yup.string().required("Enter your City").min(2),
-  country: yup.string().required("Enter your country").min(2)
+  country: yup.string().required("Enter your country").min(2),
 });
 
 const ShippingDetailsForm = () => {
@@ -36,7 +37,7 @@ const ShippingDetailsForm = () => {
       firstName: "",
       lastName: "",
       email: "",
-      number: "",
+      number: 0,
       address: "",
       zipcode: "",
       city: "",
@@ -52,113 +53,132 @@ const ShippingDetailsForm = () => {
     <div style={formDiv}>
       <form onSubmit={formik.handleSubmit}>
         <div>
-		<div 
-			style={{
-			display: 'flex',
-			flexDirection: 'row',
-			}}>
-          <TextField
-            style={textFieldStyle}
-            id="firstName"
-            name="firstName"
-            label="Förnamn"
-            fullWidth
-            value={formik.values.firstName}
-            onChange={formik.handleChange}
-            error={formik.touched.firstName && Boolean(formik.errors.firstName)}
-            helperText={formik.touched.firstName && formik.errors.firstName}
-          />
-          <TextField
-            style={textFieldStyle}
-            id="lastName"
-            name="lastName"
-            label="Efternamn"
-            fullWidth
-            value={formik.values.lastName}
-            onChange={formik.handleChange}
-            error={formik.touched.lastName && Boolean(formik.errors.lastName)}
-            helperText={formik.touched.lastName && formik.errors.lastName}
-          />
-          <TextField
-            style={textFieldStyle}
-            id="email"
-            name="email"
-            label="E-post"
-            fullWidth
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            error={formik.touched.email && Boolean(formik.errors.email)}
-            helperText={formik.touched.email && formik.errors.email}
-          />
-		  </div>
-		  	<div 
-			  style={{
-			  display: 'flex',
-			  flexDirection: 'row',
-			  }}>
-          <TextField
-            style={textFieldStyle}
-            id="number"
-            name="number"
-            label="Telefonnummer"
-            fullWidth
-            value={formik.values.number}
-            onChange={formik.handleChange}
-            error={formik.touched.number && Boolean(formik.errors.number)}
-            helperText={formik.touched.number && formik.errors.number}
-          />
-          <TextField
-            style={textFieldStyle}
-            id="address"
-            name="address"
-            label="Adress"
-            fullWidth
-            value={formik.values.address}
-            onChange={formik.handleChange}
-            error={formik.touched.address && Boolean(formik.errors.address)}
-            helperText={formik.touched.address && formik.errors.address}
-          />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
             <TextField
-            style={textFieldStyle}
-            id="zipcode"
-            name="zipcode"
-            label="Zipcode"
-            fullWidth
-            value={formik.values.zipcode}
-            onChange={formik.handleChange}
-            error={formik.touched.zipcode && Boolean(formik.errors.zipcode)}
-            helperText={formik.touched.zipcode && formik.errors.zipcode}
-          />
+              style={textFieldStyle}
+              id="firstName"
+              name="firstName"
+              label="Förnamn"
+              fullWidth
+              value={formik.values.firstName}
+              onChange={formik.handleChange}
+              error={
+                formik.touched.firstName && Boolean(formik.errors.firstName)
+              }
+              helperText={formik.touched.firstName && formik.errors.firstName}
+            />
+            <TextField
+              style={textFieldStyle}
+              id="lastName"
+              name="lastName"
+              label="Efternamn"
+              fullWidth
+              value={formik.values.lastName}
+              onChange={formik.handleChange}
+              error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+              helperText={formik.touched.lastName && formik.errors.lastName}
+            />
+            <TextField
+              style={textFieldStyle}
+              id="email"
+              name="email"
+              label="E-post"
+              fullWidth
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
           </div>
-          <div 
-			      style={{
-			      display: 'flex',
-			      flexDirection: 'row',
-			    }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
             <TextField
-            style={textFieldStyle}
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            value={formik.values.city}
-            onChange={formik.handleChange}
-            error={formik.touched.city && Boolean(formik.errors.city)}
-            helperText={formik.touched.city && formik.errors.city}
-          />
+              style={textFieldStyle}
+              id="tel"
+              name="number"
+              label="Telefonnummer"
+              type="number"
+              inputProps={{ maxLength: 10 }}
+              InputProps={{
+                disableUnderline: true,
+              }}
+              fullWidth
+              value={formik.values.number}
+              onChange={formik.handleChange}
+              error={formik.touched.number && Boolean(formik.errors.number)}
+              helperText={formik.touched.number && formik.errors.number}
+            />
             <TextField
-            style={textFieldStyle}
-            id="country"
-            name="country"
-            label="Country"
-            fullWidth
-            value={formik.values.country}
-            onChange={formik.handleChange}
-            error={formik.touched.country && Boolean(formik.errors.country)}
-            helperText={formik.touched.country && formik.errors.country}
-          />
+              style={textFieldStyle}
+              id="address"
+              name="address"
+              label="Adress"
+              fullWidth
+              value={formik.values.address}
+              onChange={formik.handleChange}
+              error={formik.touched.address && Boolean(formik.errors.address)}
+              helperText={formik.touched.address && formik.errors.address}
+            />
+            <TextField
+              style={textFieldStyle}
+              id="zipcode"
+              name="zipcode"
+              label="Zipcode"
+              inputProps={{ maxLength: 5 }}
+              InputProps={{
+                disableUnderline: true,
+              }}
+              fullWidth
+              value={formik.values.zipcode}
+              onChange={formik.handleChange}
+              error={formik.touched.zipcode && Boolean(formik.errors.zipcode)}
+              helperText={formik.touched.zipcode && formik.errors.zipcode}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <TextField
+              style={textFieldStyle}
+              id="city"
+              name="city"
+              label="City"
+              fullWidth
+              value={formik.values.city}
+              onChange={formik.handleChange}
+              error={formik.touched.city && Boolean(formik.errors.city)}
+              helperText={formik.touched.city && formik.errors.city}
+            />
+            <TextField
+              style={textFieldStyle}
+              id="country"
+              name="country"
+              label="Country"
+              fullWidth
+              value={formik.values.country}
+              onChange={formik.handleChange}
+              error={formik.touched.country && Boolean(formik.errors.country)}
+              helperText={formik.touched.country && formik.errors.country}
+            />
+          </div>
         </div>
-		    </div>
+        <div style={ButtonDiv}>
+          <Button sx={SubmitButton} type="submit" variant="contained">
+            Fortsätt
+          </Button>
+        </div>
       </form>
     </div>
   );
@@ -175,5 +195,18 @@ const formDiv: CSSProperties = {
   justifyContent: "center",
 };
 
+const ButtonDiv: CSSProperties = {
+  display: "flex",
+  justifyContent: "right",
+};
 
+const SubmitButton: CSSProperties = {
+  backgroundColor: "#BFD8D5",
+  color: "#333333",
+  marginTop: "2rem",
+  padding: "0.5rem",
+  width: "8rem",
+  boxShadow: "none",
+  textDecoration: "none",
+};
 export default ShippingDetailsForm;
--- src/context/OrderContext.tsx
+++ src/context/OrderContext.tsx
@@ -1,118 +1,118 @@
-import React, { createContext, FC, useContext, useState } from 'react';
-import { ICartItem, useCart } from './CartContext';
-import { IShippingProvider } from '../shippigProvider';
+import React, { createContext, FC, useContext, useState } from "react";
+import { ICartItem, useCart } from "./CartContext";
+import { IShippingProvider } from "../shippigProvider";
 
 interface ICustomer {
-    firstName: string,
-    lastName: string,
-    email: string,
-    number: number,
-    address: string,
-    zipcode: string,
-    city: string,
-    country: string
+  firstName: string;
+  lastName: string;
+  email: string;
+  number: string;
+  address: string;
+  zipcode: string;
+  city: string;
+  country: string;
 }
 
 interface IOrderData {
-    boughtItems: ICartItem[],
-    customer: ICustomer,
-    paymentMethod: string,
-    shippingMethod: string
+  boughtItems: ICartItem[];
+  customer: ICustomer;
+  paymentMethod: string;
+  shippingMethod: string;
 }
 
 interface IOrderContextValue {
-    order: IOrderData,
-    generateOrderId: () => number,
-    createNewOrder: (customerInfo: ICustomer) => void,
-    setShippingMethod: (shippinginfo: string) => any
+  order: IOrderData;
+  generateOrderId: () => number;
+  createNewOrder: (customerInfo: ICustomer) => void;
+  setShippingMethod: (shippinginfo: string) => any;
 }
 
 const OrderContext = createContext<IOrderContextValue>({
-    order: {
-        boughtItems: [],
-        customer: {
-            firstName: '',
-            lastName: '',
-            email: '',
-            number: 0,
-            address: '',
-            zipcode: '',
-            city: '',
-            country: ''
-        },
-        paymentMethod: '',
-        shippingMethod: ''
+  order: {
+    boughtItems: [],
+    customer: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      number: "",
+      address: "",
+      zipcode: "",
+      city: "",
+      country: "",
     },
-    generateOrderId: () => 0,
-    createNewOrder: () => {},
-    setShippingMethod: () => '',
-})
+    paymentMethod: "",
+    shippingMethod: "",
+  },
+  generateOrderId: () => 0,
+  createNewOrder: () => {},
+  setShippingMethod: () => "",
+});
 
 export function useOrderContext() {
-    return useContext(OrderContext);
+  return useContext(OrderContext);
 }
 
 export const OrderContextProvider: FC = (props) => {
-    const { cart, shipping } = useCart();
-    const [order, setOrder] = useState<IOrderData>({
-        boughtItems: [],
-        customer: {
-            firstName: '',
-            lastName: '',
-            email: '',
-            number: 0,
-            address: '',
-            zipcode: '',
-            city: '',
-            country: ''
-        },
-        paymentMethod: '',
-        shippingMethod: ''
-    });
+  const { cart, shipping } = useCart();
+  const [order, setOrder] = useState<IOrderData>({
+    boughtItems: [],
+    customer: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      number: "",
+      address: "",
+      zipcode: "",
+      city: "",
+      country: "",
+    },
+    paymentMethod: "",
+    shippingMethod: "",
+  });
 
-    const setShippingMethod = (shippingInfo: string) => {
-        setOrder({...order, shippingMethod: shippingInfo});
-        return 
-    }
+  const setShippingMethod = (shippingInfo: string) => {
+    setOrder({ ...order, shippingMethod: shippingInfo });
+    return;
+  };
 
-    const generateOrderId = () => {
-        const maxNumber = 100000;
-        const orderId = Math.floor(Math.random() * maxNumber + 1);
-        return (orderId)
-    }
+  const generateOrderId = () => {
+    const maxNumber = 100000;
+    const orderId = Math.floor(Math.random() * maxNumber + 1);
+    return orderId;
+  };
 
-    const createNewOrder = (customerInfo: ICustomer) => {
-        console.log(customerInfo);
-        const boughtItems = [...cart];
-         const customer: ICustomer = {
-             firstName: customerInfo.firstName,
-             lastName: customerInfo.lastName,
-             email: customerInfo.email,
-             number: customerInfo.number,
-             address: customerInfo.address,
-             zipcode: customerInfo.zipcode,
-             city: customerInfo.city,
-             country: customerInfo.country
-         };
-         let updatedOrder: IOrderData = {
-                 boughtItems: boughtItems,
-                 customer: customer,
-                 paymentMethod: order.paymentMethod,
-                 shippingMethod: order.shippingMethod,
-         }
-         setOrder(updatedOrder);
-        
-    }
+  const createNewOrder = (customerInfo: ICustomer) => {
+    console.log(customerInfo);
+    const boughtItems = [...cart];
+    const customer: ICustomer = {
+      firstName: customerInfo.firstName,
+      lastName: customerInfo.lastName,
+      email: customerInfo.email,
+      number: customerInfo.number,
+      address: customerInfo.address,
+      zipcode: customerInfo.zipcode,
+      city: customerInfo.city,
+      country: customerInfo.country,
+    };
+    let updatedOrder: IOrderData = {
+      boughtItems: boughtItems,
+      customer: customer,
+      paymentMethod: order.paymentMethod,
+      shippingMethod: order.shippingMethod,
+    };
+    setOrder(updatedOrder);
+  };
 
-    return (
-        <OrderContext.Provider
-            value={{
-                order,
-                generateOrderId,
-                createNewOrder,
-                setShippingMethod
-            }}>
-                {props.children}
-        </OrderContext.Provider>
-    )
-}
+  return (
+    <OrderContext.Provider
+      value={{
+        order,
+        generateOrderId,
+        createNewOrder,
+        setShippingMethod,
+      }}
+    >
+      {props.children}
+    </OrderContext.Provider>
+  );
+};
