fe3614c80bc6761d5ee252f725156a41b12592cd
Emil <emil.hagelin@medieinstitutet.se>
2022-03-30 10:34:26 +0200

    camelcase problem

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,21 @@
-import React, { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import CartProvider, { useCart } from './context/CartContext';
+import React, { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
+import CartProvider, { useCart } from "./context/CartContext";
 
-import Layout from './layout/layout';
+import Layout from "./layout/layout";
 
 function App() {
-	const { cart } = useCart();
+  const { cart } = useCart();
 
-	return (
-		<StrictMode>
-			<BrowserRouter>
-				<CartProvider>
-					<Layout />
-				</CartProvider>
-			</BrowserRouter>
-		</StrictMode>
-	);
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <CartProvider>
+          <Layout />
+        </CartProvider>
+      </BrowserRouter>
+    </StrictMode>
+  );
 }
 
 export default App;
